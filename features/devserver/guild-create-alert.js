7b5db43b4be91018a0396762da52183cd2d7e875
Xekso <timmaxgibson@gmail.com>
2021-02-19 21:50:25 +1100

    add new server alert

---
+++ features/devserver/guild-create-alert.js
@@ -0,0 +1,14 @@
+const Embed = require("@configurations/embed-construct")
+
+module.exports = (client) => {
+    client.on('guildCreate', async (newGuild) => {
+        const systemChannel = await client.channels.cache.get(process.env.SYSTEM_ID)
+        systemChannel.send(Embed.guildCreate(newGuild))
+    })
+}
+
+module.exports.config = {
+    displayName: 'Log when bot joins new server',
+    dbName: 'GUILD-ALERT',
+    loadDBFirst: false
+}
